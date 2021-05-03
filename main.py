6ad6834c2344f74ebe3f32800d9a7916369dc916
Garth Johnson <growlf@thenetyeti.com>
2021-05-02 18:13:40 -0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def on_forever():
     moisturelvl = 0
     if pins.digital_read_pin(DigitalPin.P0) <= 0:
         basic.show_icon(IconNames.UMBRELLA)
-        control.wait_micros(4000)
+    control.wait_micros(4000)
     basic.show_icon(IconNames.HOUSE)
     print(moisturelvl)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let moisturelvl = 0
 basic.forever(function on_forever() {
     
+    moisturelvl = 0
+    if (pins.digitalReadPin(DigitalPin.P0) <= 0) {
+        basic.showIcon(IconNames.Umbrella)
+    }
+    
+    control.waitMicros(4000)
+    basic.showIcon(IconNames.House)
+    console.log(moisturelvl)
 })
