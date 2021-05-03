df13803e772feb33da899851eef202541ce5da6a
Garth Johnson <growlf@thenetyeti.com>
2021-05-02 18:13:09 -0700

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,11 @@
+moisturelvl = 0
+
+def on_forever():
+    global moisturelvl
+    moisturelvl = 0
+    if pins.digital_read_pin(DigitalPin.P0) <= 0:
+        basic.show_icon(IconNames.UMBRELLA)
+        control.wait_micros(4000)
+    basic.show_icon(IconNames.HOUSE)
+    print(moisturelvl)
+basic.forever(on_forever)
