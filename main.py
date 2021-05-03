96aa3b8472dafcc6d0405433fe594811411d4eea
Garth Johnson <growlf@thenetyeti.com>
2021-05-02 21:56:16 -0700

    variable thresholds with menu

--- main.py
+++ main.py
@@ -1,17 +1,56 @@
-moisturelvl = 0
-threshold = 0
-threshold = 200
+# mode
+# 0 = running mode
+# 1 = lower thresshold setting
+# 2 = upper thresshold setting
 
 def on_button_pressed_a():
-    global threshold
-    threshold = threshold - 5
-    basic.show_number(threshold)
+    global mode, threshold_lower, threshold_upper
+    if mode == 0:
+        mode = 1
+    elif mode == 1:
+        # If running, switch to lower threshold editing
+        # If lower-edit mode, decrease threshold
+        threshold_lower = threshold_lower - 5
+        basic.show_number(threshold_lower)
+    elif mode == 2:
+        # If upper-edit mode, decrease threshold
+        threshold_upper = threshold_upper - 5
+        basic.show_number(threshold_upper)
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global mode
+    if mode != 0:
+        mode = 0
+    basic.show_number(moisturelvl)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    global mode, threshold_lower, threshold_upper
+    if mode == 0:
+        mode = 1
+    elif mode == 1:
+        # If running, switch to lower threshold editing
+        # If lower-edit mode, increase threshold
+        threshold_lower = threshold_lower + 5
+        basic.show_number(threshold_lower)
+    elif mode == 2:
+        # If upper-edit mode, increase threshold
+        threshold_upper = threshold_upper + 5
+        basic.show_number(threshold_upper)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+moisturelvl = 0
+mode = 0
+threshold_lower = 0
+threshold_upper = 0
+threshold_upper = 400
+threshold_lower = 200
 
 def on_forever():
     global moisturelvl
     moisturelvl = pins.analog_read_pin(AnalogPin.P0)
-    if moisturelvl <= threshold:
+    if moisturelvl <= threshold_lower:
         basic.show_icon(IconNames.UMBRELLA)
     else:
         basic.show_icon(IconNames.HOUSE)
--- main.ts
+++ main.ts
@@ -1,15 +1,57 @@
-let moisturelvl = 0
-let threshold = 0
-threshold = 200
+//  mode
+//  0 = running mode
+//  1 = lower thresshold setting
+//  2 = upper thresshold setting
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    threshold = threshold - 5
-    basic.showNumber(threshold)
+    if (mode == 0) {
+        mode = 1
+    } else if (mode == 1) {
+        //  If running, switch to lower threshold editing
+        //  If lower-edit mode, decrease threshold
+        threshold_lower = threshold_lower - 5
+        basic.showNumber(threshold_lower)
+    } else if (mode == 2) {
+        //  If upper-edit mode, decrease threshold
+        threshold_upper = threshold_upper - 5
+        basic.showNumber(threshold_upper)
+    }
+    
 })
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    if (mode != 0) {
+        mode = 0
+    }
+    
+    basic.showNumber(moisturelvl)
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (mode == 0) {
+        mode = 1
+    } else if (mode == 1) {
+        //  If running, switch to lower threshold editing
+        //  If lower-edit mode, increase threshold
+        threshold_lower = threshold_lower + 5
+        basic.showNumber(threshold_lower)
+    } else if (mode == 2) {
+        //  If upper-edit mode, increase threshold
+        threshold_upper = threshold_upper + 5
+        basic.showNumber(threshold_upper)
+    }
+    
+})
+let moisturelvl = 0
+let mode = 0
+let threshold_lower = 0
+let threshold_upper = 0
+threshold_upper = 400
+threshold_lower = 200
 basic.forever(function on_forever() {
     
     moisturelvl = pins.analogReadPin(AnalogPin.P0)
-    if (moisturelvl <= threshold) {
+    if (moisturelvl <= threshold_lower) {
         basic.showIcon(IconNames.Umbrella)
     } else {
         basic.showIcon(IconNames.House)
