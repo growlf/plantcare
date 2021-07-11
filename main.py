833aca9e165db723d6113a929c55c561a62f4647
Garth Johnson <growlf@thenetyeti.com>
2021-07-11 16:28:54 -0700

    fixed threshold setting menues

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-# modes
-# 0 = running mode
-# 1 = lower thresshold setting
-# 2 = upper thresshold setting
+# running_modes
+# 0 = constant/curent display
+# 1 = lower thresshold menu
+# 2 = upper thresshold menu
 
 def on_button_pressed_a():
-    global mode, threshold_lower, threshold_upper
-    if mode == 0:
-        # If running mode, switch to threshold_lower editing
+    global running_mode, lower_threshold, upper_threshold
+    # When the A button is pressed, either select the lowerthreshold menu running_mode
+    # or decrease the threshold of the running_mode that is currently selected
+    if running_mode == 0:
+        # If running running_mode, switch to lower_threshold editing and display an indicator
         basic.show_leds("""
             # # # # #
                         . # # # .
@@ -14,28 +16,28 @@ def on_button_pressed_a():
                         . . . . .
                         # # # # #
         """)
-        mode = 1
-    elif mode == 1:
-        # If lower-edit mode, decrease threshold
-        threshold_lower = threshold_lower - 5
-        led.plot_bar_graph(threshold_lower, moisture_max)
-    elif mode == 2:
-        # If upper-edit mode, decrease threshold
-        threshold_upper = threshold_upper - 5
-        led.plot_bar_graph(threshold_upper, moisture_max)
+        running_mode = 1
+        basic.pause(1000)
+    elif running_mode == 1:
+        # If lower-edit running_mode, decrease lower threshold
+        lower_threshold = lower_threshold - 5
+    elif running_mode == 2:
+        # If upper-edit running_mode, decrease upper threshold
+        upper_threshold = upper_threshold - 5
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    global mode
-    if mode != 0:
-        mode = 0
-    led.plot_bar_graph(moisturelvl, moisture_max)
+    global running_mode
+    if running_mode != 0:
+        running_mode = 0
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
-    global mode, threshold_lower, threshold_upper
-    if mode == 0:
-        # If running mode, switch to threshold_upper editing
+    global running_mode, lower_threshold, upper_threshold
+    # When the B button is pressed, either select the upper threshold menu running_mode
+    # or increase the threshold of the running_mode that is currently selected
+    if running_mode == 0:
+        # If running running_mode, switch to upper_threshold editing and display an indicator
         basic.show_leds("""
             # # # # #
                         . . . . .
@@ -43,39 +45,41 @@ def on_button_pressed_b():
                         . # # # .
                         # # # # #
         """)
-        mode = 1
-    elif mode == 1:
-        # If lower-edit mode, increase threshold
-        threshold_lower = threshold_lower + 5
-        led.plot_bar_graph(threshold_lower, moisture_max)
-    elif mode == 2:
-        # If upper-edit mode, increase threshold
-        threshold_upper = threshold_upper + 5
-        led.plot_bar_graph(threshold_upper, moisture_max)
+        running_mode = 2
+        basic.pause(1000)
+    elif running_mode == 1:
+        # If lower-edit running_mode, increase lower threshold
+        lower_threshold = lower_threshold + 5
+    elif running_mode == 2:
+        # If upper-edit running_mode, increase upper threshold
+        upper_threshold = upper_threshold + 5
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+moisture_graph_value = 0
+moisture_graph_range = 0
 moisturelvl = 0
-moisture_range = 0
-moisture_graph = 0
-mode = 0
-moisture_max = 0
-threshold_upper = 600
-threshold_lower = 500
+running_mode = 0
+lower_threshold = 0
+upper_threshold = 0
+upper_threshold = 600
+lower_threshold = 500
 moisture_max = 950
 delay_ms = 1000
-#inValue = pins.i2c_read_number(36, NumberFormat.UINT16_BE, False)
 
 def on_forever():
-    global moisturelvl
-    global moisture_range
+    global moisturelvl, moisture_graph_range, moisture_graph_value
+    # Read the moisture sensor
     moisturelvl = pins.analog_read_pin(AnalogPin.P0)
-    moisture_range = threshold_upper-threshold_lower
-    moisture_graph = ((moisturelvl - threshold_lower) / moisture_range) * moisture_range
-    if mode == 0:
-        if moisturelvl <= threshold_lower:
+    # Calculate the range (in case it has changed)
+    moisture_graph_range = upper_threshold - lower_threshold
+    moisture_graph_value = moisturelvl - lower_threshold
+    if running_mode == 0:
+        # In running running_mode, check if the moisture is in range and alert if it is not
+        if moisture_graph_value <= 0:
             basic.show_icon(IconNames.UMBRELLA)
             soundExpression.twinkle.play_until_done()
-        else:
-            led.plot_bar_graph(moisture_graph, moisture_range)
+    # In other running_modes, display the current moisture in the context of the specified moisture_graph_range
+    led.plot_bar_graph(moisture_graph_value, moisture_graph_range)
+    # Pause to leave the display on long enough to be seen
     basic.pause(delay_ms)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,11 +1,13 @@
-//  modes
-//  0 = running mode
-//  1 = lower thresshold setting
-//  2 = upper thresshold setting
+//  running_modes
+//  0 = constant/curent display
+//  1 = lower thresshold menu
+//  2 = upper thresshold menu
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    if (mode == 0) {
-        //  If running mode, switch to threshold_lower editing
+    //  When the A button is pressed, either select the lowerthreshold menu running_mode
+    //  or decrease the threshold of the running_mode that is currently selected
+    if (running_mode == 0) {
+        //  If running running_mode, switch to lower_threshold editing and display an indicator
         basic.showLeds(`
             # # # # #
                         . # # # .
@@ -13,30 +15,30 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
                         . . . . .
                         # # # # #
         `)
-        mode = 1
-    } else if (mode == 1) {
-        //  If lower-edit mode, decrease threshold
-        threshold_lower = threshold_lower - 5
-        led.plotBarGraph(threshold_lower, moisture_max)
-    } else if (mode == 2) {
-        //  If upper-edit mode, decrease threshold
-        threshold_upper = threshold_upper - 5
-        led.plotBarGraph(threshold_upper, moisture_max)
+        running_mode = 1
+        basic.pause(1000)
+    } else if (running_mode == 1) {
+        //  If lower-edit running_mode, decrease lower threshold
+        lower_threshold = lower_threshold - 5
+    } else if (running_mode == 2) {
+        //  If upper-edit running_mode, decrease upper threshold
+        upper_threshold = upper_threshold - 5
     }
     
 })
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     
-    if (mode != 0) {
-        mode = 0
+    if (running_mode != 0) {
+        running_mode = 0
     }
     
-    led.plotBarGraph(moisturelvl, moisture_max)
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    if (mode == 0) {
-        //  If running mode, switch to threshold_upper editing
+    //  When the B button is pressed, either select the upper threshold menu running_mode
+    //  or increase the threshold of the running_mode that is currently selected
+    if (running_mode == 0) {
+        //  If running running_mode, switch to upper_threshold editing and display an indicator
         basic.showLeds(`
             # # # # #
                         . . . . .
@@ -44,43 +46,45 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
                         . # # # .
                         # # # # #
         `)
-        mode = 1
-    } else if (mode == 1) {
-        //  If lower-edit mode, increase threshold
-        threshold_lower = threshold_lower + 5
-        led.plotBarGraph(threshold_lower, moisture_max)
-    } else if (mode == 2) {
-        //  If upper-edit mode, increase threshold
-        threshold_upper = threshold_upper + 5
-        led.plotBarGraph(threshold_upper, moisture_max)
+        running_mode = 2
+        basic.pause(1000)
+    } else if (running_mode == 1) {
+        //  If lower-edit running_mode, increase lower threshold
+        lower_threshold = lower_threshold + 5
+    } else if (running_mode == 2) {
+        //  If upper-edit running_mode, increase upper threshold
+        upper_threshold = upper_threshold + 5
     }
     
 })
+let moisture_graph_value = 0
+let moisture_graph_range = 0
 let moisturelvl = 0
-let moisture_range = 0
-let moisture_graph = 0
-let mode = 0
-let moisture_max = 0
-let threshold_upper = 600
-let threshold_lower = 500
-moisture_max = 950
+let running_mode = 0
+let lower_threshold = 0
+let upper_threshold = 0
+upper_threshold = 600
+lower_threshold = 500
+let moisture_max = 950
 let delay_ms = 1000
-// inValue = pins.i2c_read_number(36, NumberFormat.UINT16_BE, False)
 basic.forever(function on_forever() {
     
-    
+    //  Read the moisture sensor
     moisturelvl = pins.analogReadPin(AnalogPin.P0)
-    moisture_range = threshold_upper - threshold_lower
-    let moisture_graph = (moisturelvl - threshold_lower) / moisture_range * moisture_range
-    if (mode == 0) {
-        if (moisturelvl <= threshold_lower) {
+    //  Calculate the range (in case it has changed)
+    moisture_graph_range = upper_threshold - lower_threshold
+    moisture_graph_value = moisturelvl - lower_threshold
+    if (running_mode == 0) {
+        //  In running running_mode, check if the moisture is in range and alert if it is not
+        if (moisture_graph_value <= 0) {
             basic.showIcon(IconNames.Umbrella)
             soundExpression.twinkle.playUntilDone()
-        } else {
-            led.plotBarGraph(moisture_graph, moisture_range)
         }
         
     }
     
+    //  In other running_modes, display the current moisture in the context of the specified moisture_graph_range
+    led.plotBarGraph(moisture_graph_value, moisture_graph_range)
+    //  Pause to leave the display on long enough to be seen
     basic.pause(delay_ms)
 })
